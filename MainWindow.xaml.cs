1e550b7a623f59e0278c3f60c448c460b4df11a6
MAXDORPH <125501153+MAXDORPH@users.noreply.github.com>
2023-04-30 14:14:56 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace how_to_pid
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+            SelfUserElement.Width = 400;
+            SelfUserElement.Height = 300;
+            SelfGrid.Width = 400;
+            SelfGrid.Height = 400;
+        }
+
+        private void enlarge_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (SelfUserElement.Height == 200)
+            {
+                SelfUserElement.Height = 300;
+                SelfGrid.Width = 300;
+                SelfGrid.Height = 300;
+            }
+            else
+            {
+                SelfUserElement.Height = 200;
+                SelfGrid.Width = 300;
+                SelfGrid.Height = 300;
+            }
+        }
+        private void Rect_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (SelfUserElement.Height == 200)
+            {
+                SelfUserElement.Height = 300;
+                SelfGrid.Width = 300;
+                SelfGrid.Height = 300;
+            }
+            else
+            {
+                SelfUserElement.Height = 200;
+                SelfGrid.Width = 300;
+                SelfGrid.Height = 300;
+            }
+        }
+    }
+}
