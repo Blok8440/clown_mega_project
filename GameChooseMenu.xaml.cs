ba00c63efb259466be9d1608dbc6a458bd848435
ironkrendel <v.a.ryshkov@gmail.com>
2023-05-03 22:20:36 +0300

    Сделал 2 заготовки главного меню и создания игры
    Добавил два файла. Один - это страница с главным меню, второй - это окно для создания игры.

---
+++ GameChooseMenu.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace clown_mega_project
+{
+    /// <summary>
+    /// Логика взаимодействия для GameChooseMenu.xaml
+    /// </summary>
+    public partial class GameChooseMenu : Page
+    {
+        public GameChooseMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateNewGame_btn_Click(object sender, RoutedEventArgs e)
+        {
+            var creation_window = new GameCreateWindow();
+            creation_window.Show();
+            creation_window.Activate();
+        }
+    }
+}
